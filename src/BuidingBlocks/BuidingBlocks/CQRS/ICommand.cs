cef55389568952f70dfa11fb8371b827fa4b53f8
hungnqph19112 <nqh1703k2@gmail.com>
2025-02-18 16:57:16 +0700

    Building Blocks - Abstact CQRS

---
+++ src/BuidingBlocks/BuidingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace BuidingBlocks.CQRS
+{
+
+    public interface ICommand : ICommand<Unit>
+    {
+         
+    }
+
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+
+    }
+}
