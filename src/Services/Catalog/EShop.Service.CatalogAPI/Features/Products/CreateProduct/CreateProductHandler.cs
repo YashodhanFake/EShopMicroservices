cfe81faf7d8cedd73dc35f67e36ba54938ca26b8
hungnqph19112 <nqh1703k2@gmail.com>
2025-02-18 16:57:36 +0700

    Catalog Service first API

---
+++ src/Services/Catalog/EShop.Service.CatalogAPI/Features/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,28 @@
+﻿using BuidingBlocks.CQRS;
+using EShop.Service.CatalogAPI.Domain.Entities;
+
+namespace EShop.Service.CatalogAPI.Features.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageUrl, decimal Price) : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //Business logic here
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageUrl = command.ImageUrl,
+                Price = command.Price,
+            };
+
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
--- src/Services/Catalog/EShop.Service.CatalogAPI/Program.cs
+++ src/Services/Catalog/EShop.Service.CatalogAPI/Program.cs
@@ -1,9 +1,21 @@
+global using Carter;
+global using Mapster;
+global using MediatR;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
 
 //Configure the HTTPs request pineline
+
+app.MapCarter();
 
 app.Run();
